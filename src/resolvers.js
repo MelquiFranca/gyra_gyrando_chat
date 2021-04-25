aeec25ed27d00bad082919e5ff0cc830c745ffe7
Melquisedeque P. França <melquifranca@hotmail.com>
2021-04-24 22:51:44 -0300

    Correção nas Subscriptions

--- src/resolvers.js
+++ src/resolvers.js
@@ -9,29 +9,29 @@ export default {
         me: (_, __, { dataSources }) => dataSources.usuarioAPI.loginUsuario(),
     },
     Mutation: {
-        login: (_, { nome, tipo }, { dataSources }) => {
-            pubsub.publish('USUARIO_LOGADO', { entradaUsuario: { nome, tipo } })
-            return dataSources.usuarioAPI.loginUsuario({ nome, tipo })
+        login: async (_, { nome, tipo }, { dataSources }) => {
+            const retorno = await dataSources.usuarioAPI.loginUsuario({ nome, tipo })
+            return pubsub.publish('USUARIO_LOGADO', { entradaUsuario: retorno })
         },
 
-        novaMensagem: (_, { usuarioId, conteudo }, { dataSources }) => {
-            const retorno = dataSources.mensagemAPI.novaMensagem({ usuarioId, conteudo })
-            pubsub.publish('USUARIO_DESLOGADO', retorno)
-            return retorno
+        novaMensagem: async(_, { usuarioId, conteudo }, { dataSources }) => {
+            const retorno = await dataSources.mensagemAPI.novaMensagem({ usuarioId, conteudo })
+            return pubsub.publish('NOVA_MENSAGEM', {atualizarMensagens: retorno})
         },        
-        logoff: (_, { usuarioId }, { dataSources }) => {
-            dataSources.usuarioAPI.logoffUsuario({usuarioId})
+        logoff: async (_, { usuarioId }, { dataSources }) => {
+            const retorno = await dataSources.usuarioAPI.logoffUsuario({usuarioId})
+            return pubsub.publish('USUARIO_DESLOGADO', retorno)
         },
     },
     Subscription: {
         entradaUsuario: {
-            subscribe:  pubsub.asyncIterator(['USUARIO_LOGADO'])
+            subscribe: _ =>  pubsub.asyncIterator(['USUARIO_LOGADO'])
         },
         saidaUsuario: {
-            subscribe:  pubsub.asyncIterator(['USUARIO_DESLOGADO'])
+            subscribe:  _ => pubsub.asyncIterator(['USUARIO_DESLOGADO'])
         },
         atualizarMensagens: {
-            subscribe:  pubsub.asyncIterator(['NOVA_MENSAGEM'])
+            subscribe:  _ => pubsub.asyncIterator(['NOVA_MENSAGEM'])
         },
     }
 }
